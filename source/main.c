10b1f5a58b13e92c781cd812f782ad5dd5848b69
Alexander Trejo <atrej013@wch136-24.cs.ucr.edu>
2021-10-02 00:29:10 -0700

    Prelab

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: atrej013
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 023
+ *	Assignment: Lab #2  Exercise #Prelab
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -14,10 +14,19 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+    DDRB = 0xFF; PORTB = 0xFF; // Configure port B's 8 pins as outputs, initialize to 0s
     /* Insert your solution below */
+    unsigned char tmpB = 0x00;
+    unsigned char tmpA = 0x00;
     while (1) {
-
+	tmpA = PINA & 0x01;
+	if (tmpA == 0x01) {
+		tmpB = (tmpB & 0xFC) | 0x01;
+	} else {
+		tmpB = (tmpB & 0xFC) | 0x02;
+	}
+	PORTB = tmpB;
     }
-    return 1;
+    return 0;
 }
